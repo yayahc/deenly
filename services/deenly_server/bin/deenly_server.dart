03509ceb1315375592beefeda6b743988aaa1769
Yaya HC <hamedcuenca5@gmail.com>
2026-01-12 08:13:16 +0000

    chore: init dart frog server

--- services/deenly_server/bin/deenly_server.dart
+++
@@ -1,5 +0,0 @@
-import 'package:deenly_server/deenly_server.dart' as deenly_server;
-
-void main(List<String> arguments) {
-  print('Hello world: ${deenly_server.calculate()}!');
-}
--- services/deenly_server/test/deenly_server_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:deenly_server/deenly_server.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
